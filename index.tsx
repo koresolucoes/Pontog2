24a6f4eb7c8d297dc8d38340ea31a739ff646d93
KoreSolucoes <koresoluciones@outlook.com>
2026-09-07 14:54:20 -0300

    feat(ux): load Ponto G visual language globally

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './i18n';
+import './styles/ponto-g.css';
 import App from './App';
 import { installLeafletBasemapAdapter } from './engines/maps/public';
 
